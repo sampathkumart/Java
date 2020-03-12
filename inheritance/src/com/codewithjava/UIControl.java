c6dd563b5cc68b8182f54b67905dcf02fa4a1673
sampathkumar <semsampathkumar@gmail.com>
2020-03-12 20:45:13 +0530

    inheritance

---
+++ inheritance/src/com/codewithjava/UIControl.java
@@ -0,0 +1,17 @@
+package com.codewithjava;
+
+public class UIControl {
+public boolean isEnabled = true;
+
+public void enable(){
+    isEnabled = true;
+}
+
+public void disable(){
+    isEnabled =false;
+}
+public boolean isEnable(){
+    return isEnabled;
+}
+
+}
--- procedural programming/src/com/codewithjava/Main.java
+++ procedural programming/src/com/codewithjava/Main.java
@@ -3,12 +3,9 @@ package com.codewithjava;
 public class Main {
 
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.salary = 20_000;
-        employee.rate = 25;
-        int wage  = employee.calculateWage(extraHours: 10);
-
+        var employee = new Employee(50_000, 20);
+        Employee.printNumberOfEmployees();
+        int wage = employee.calculateWage();
         System.out.println(wage);
-
     }
 }
